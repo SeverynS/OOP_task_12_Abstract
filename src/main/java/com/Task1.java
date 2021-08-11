f267434cd672e9e57e50968e819bc0598a01616d
SeverynZ <s.zlochovsky@gmail.com>
2021-08-11 14:07:25 +0300

    main method optimization

--- src/main/java/com/Task1.java
+++ src/main/java/com/Task1.java
@@ -3,22 +3,17 @@ package com;
 /*1) Сделайте иерархию по геометрическим фигурам(базовый класс фигура) абстрактные методы вычисления площади/периметра
 и конкретные классы треугольник, прямоугольник и т.д.*/
 
+import com.models.figures.Figure;
 import com.models.figures.Hexagon;
 import com.models.figures.Rectangle;
 import com.models.figures.Triangle;
 
 public class Task1 {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(5, 2);
-        rectangle.calculateArea();
-        rectangle.calculatePerimeter();
-
-        Triangle triangle = new Triangle(9, 5, 8);
-        triangle.calculateArea();
-        triangle.calculatePerimeter();
-
-        Hexagon hexagon = new Hexagon(10);
-        hexagon.calculateArea();
-        hexagon.calculatePerimeter();
+        Figure[] figures = {new Hexagon(9), new Rectangle(3, 8), new Triangle(9, 5, 8)};
+        for(Figure f : figures){
+            f.calculateArea();
+            f.calculatePerimeter();
+        }
     }
 }
